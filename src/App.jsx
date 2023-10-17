63e60fb18aae96d95ea17158d25a0fc0a024ee08
teerthraj_verma <teerthrajverma181001@gmail.com>
2023-10-17 14:19:36 +0530

    added components in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import classes from "./App.module.css";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Nav from "./components/Nav/Nav";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Experience from "./components/Experience/Experience";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <Header />
+      <About />
+      <Contact />
+      <Nav />
+      <Portfolio />
+      <Experience />
+    </Fragment>
+  );
 };
 
 export default App;
