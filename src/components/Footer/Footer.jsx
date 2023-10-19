430e0b299ada8807c70fa2e3829ba2afe2ce9b50
teerthraj_verma <teerthrajverma181001@gmail.com>
2023-10-19 21:41:53 +0530

    added footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,49 @@
 import React, { Fragment } from "react";
 import classes from "./Footer.module.css";
 
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
 const Footer = () => {
   return (
     <Fragment>
-      <section id="footer" className={classes.footer}>
-        Footer
-      </section>
+      <footer>
+        <a href="#" className={classes.footer__logo}>
+          Teerthraj
+        </a>
+        <ul>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div className={classes.footer__socials}>
+          <a href="https://facebook.com">
+            <FaFacebookF />
+          </a>
+          <a href="https://instagram.com">
+            <FiInstagram />
+          </a>
+          <a href="https://twitter.com">
+            <IoLogoTwitter />
+          </a>
+        </div>
+        <div className={classes.footer__copyright}>
+          <small>&copy; Teerthraj. All right reserved. </small>
+        </div>
+      </footer>
     </Fragment>
   );
 };
